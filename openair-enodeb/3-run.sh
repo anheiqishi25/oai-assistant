a17f4b6cdc50ec0fa7be2192172830f74646b390
anheiqishi25 <33386509+anheiqishi25@users.noreply.github.com>
2021-01-02 22:13:57 +0800

    Create 3-run.sh

---
+++ openair-enodeb/3-run.sh
@@ -0,0 +1,4 @@
+ip address
+echo "Please input network interface name [eg:eth0]:"&& read eth
+echo "Please input EPC ip address [eg:192.168.23.130]:" && read epc
+sudo ip route add 192.168.61.0/24 via $epc dev $eth
