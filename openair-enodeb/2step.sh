85c2c7ca2c91a562bb38a476d90d19fbebb8ff52
anheiqishi25 <33386509+anheiqishi25@users.noreply.github.com>
2021-01-02 20:08:50 +0800

    Update 2step.sh

--- openair-enodeb/2step.sh
+++ openair-enodeb/2step.sh
@@ -1,5 +1,9 @@
 cd ~/oai/openairinterface5g/
+cwd=`pwd`
 source oaienv
 cd cmake_targets/
+echo "Please input proxy address[eg: http://192.168.50.102:52578]"
+read proxy
+egrep -lRZ "uhd_images_downloader" $cwd | xargs -o -l sed -i -e "s|uhd_images_downloader$|uhd_images_downloader --http-proxy $proxy|g"
 ./build_oai -I -w USRP --eNB --UE
 
